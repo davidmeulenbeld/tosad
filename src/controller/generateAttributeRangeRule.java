30b616a377e8cc5040429a179006cfb9f155fd25
Dannymos <dannymoswow@gmail.com>
2018-01-15 13:19:39 +0100

    Created generateAttributeRangeRule

---
+++ src/controller/generateAttributeRangeRule.java
@@ -0,0 +1,12 @@
+package controller;
+
+import domain.attributeRangeRule;
+import domain.BusinessRule;
+
+public class generateAttributeRangeRule {
+	
+	public String createAttributeRangeRule() {
+		return "asd";
+	}
+
+}
