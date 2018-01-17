fa623413398cae0ffd79aec829a81a33e9065222
Dannymos <dannymoswow@gmail.com>
2018-01-17 16:19:12 +0100

    generateAttributeRangeRule toegevoegd

--- src/controller/generateAttributeRangeRule.java
+++ src/controller/generateAttributeRangeRule.java
@@ -5,8 +5,34 @@ import domain.BusinessRule;
 
 public class generateAttributeRangeRule {
 	
-	public String createAttributeRangeRule() {
-		return "asd";
+	//generalControllerFunctions gtws = new generalControllerFunctions();
+	
+	public String decideTypeGeneratedAttrRang(attributeRangeRule rule) { 
+		String result;
+		
+		if (rule.isConstraint()) {
+			result = createAttributeRangeRuleConstraint(rule);
+		}
+		else {
+			result = createAttributeRangeRuleTrigger(rule);
+		}
+		
+		return result;
+		
+	}
+	
+	public String createAttributeRangeRuleTrigger(attributeRangeRule rule) {
+		String trigger = "CREATE OR REPLACE TRIGGER";
+		gtws.generateTriggerWhenString(compareRule.isInsert(), compareRule.isDelete(), compareRule.isUpdate(), compareRule.getMainTable(), compareRule.getAffectedColumn());
+		return trigger;
+	}
+	
+	public String createAttributeRangeRuleConstraint(attributeRangeRule rule) {
+		String constraint = "ALTER TABLE";
+		constraint += rule.getMainTable();
+		constraint += "add constraint " + rule.getName() + " check " + rule.getAffectedColumn() + " BETWEEN "
+                + rule.getRangeStart() + " AND " + rule.getRangeEnd();
+		return constraint;
 	}
 
 }
