3191304bd9932a5dc7a3871f01b7ec33f5495d2e
patrick kottman <patkottman@gmail.com>
2018-01-30 12:38:04 +0100

    some javadoc

--- src/services/Roothandler.java
+++ src/services/Roothandler.java
@@ -7,13 +7,14 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ *  handles root
+ */
 public class Roothandler implements HttpHandler{
     @Override
 
     public void handle(HttpExchange he) throws IOException {
-        BusinessRuleService brs = new BusinessRuleService();
 
-        brs.getBusinessRules();
         String response = "<h1>Server start success " +
                 "if you see this message</h1>" + "<h1>Port: " + "</h1>";
         he.sendResponseHeaders(200, response.length());
